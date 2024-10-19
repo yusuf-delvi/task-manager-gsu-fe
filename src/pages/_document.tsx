8443a7a7bceb936a417ebcf24a69ee21e5b886e2
yusuf-delvi <yusufmddelvi8@gmail.com>
2024-10-19 17:54:13 +0530

    configure tailwind forms and colors changes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html lang='en'>
+			<Head>
+				<link rel='stylesheet' href='https://rsms.me/inter/inter.css' />
+			</Head>
+			<body className='antialiased h-full'>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
+			},
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+			},
+		},
+	},
+	// eslint-disable-next-line @typescript-eslint/no-require-imports
+	plugins: [require('@tailwindcss/forms')],
 };
 export default config;

c59ff4d340c9fd9656e09d10854337fe6800b582
yusuf-delvi <yusufmddelvi8@gmail.com>
2024-10-19 18:07:00 +0530

    signup page

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,12 +1,12 @@
 import { FormEvent } from 'react';
 import { useRouter } from 'next/router';
-import Link from 'next/link';
 import { useAuth } from '@/context/AuthContext';
 import { LoginResponse } from '@/types';
+import Link from 'next/link';
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-export default function LoginPage() {
+export default function SignupPage() {
 	const router = useRouter();
 	const { saveJWTToken } = useAuth();
 
@@ -14,13 +14,14 @@ export default function LoginPage() {
 		event.preventDefault();
 
 		const formData = new FormData(event.currentTarget);
+		const name = formData.get('name');
 		const email = formData.get('email');
 		const password = formData.get('password');
 
-		const response = await fetch(`${API_BASE_URL}/login`, {
+		const response = await fetch(`${API_BASE_URL}/signup`, {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ email, password }),
+			body: JSON.stringify({ name, email, password }),
 		});
 
 		if (response.ok) {
@@ -45,12 +46,33 @@ export default function LoginPage() {
 			<div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
 				<div className='sm:mx-auto sm:w-full sm:max-w-sm'>
 					<h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
-						Sign in to your account
+						Register your account
 					</h2>
 				</div>
 
 				<div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
 					<form onSubmit={handleSubmit} className='space-y-6'>
+						<div>
+							<div className='flex items-center justify-between'>
+								<label
+									htmlFor='name'
+									className='block text-sm font-medium leading-6 text-gray-900'
+								>
+									Full name
+								</label>
+							</div>
+							<div className='mt-2'>
+								<input
+									id='name'
+									name='name'
+									type='text'
+									required
+									autoComplete='name'
+									className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+								/>
+							</div>
+						</div>
+
 						<div>
 							<label
 								htmlFor='email'
@@ -96,18 +118,18 @@ export default function LoginPage() {
 								type='submit'
 								className='flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
 							>
-								Sign in
+								Register
 							</button>
 						</div>
 					</form>
 
 					<p className='mt-10 text-center text-sm text-gray-500'>
-						Not Registered?{' '}
+						Already Registered?{' '}
 						<Link
-							href='/signup'
+							href='/login'
 							className='font-semibold leading-6 text-indigo-600 hover:text-indigo-500'
 						>
-							Register
+							Login
 						</Link>
 					</p>
 				</div>

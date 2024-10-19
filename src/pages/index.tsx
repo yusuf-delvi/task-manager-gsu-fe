242cf7db2f736032bf5d16c47cc7902cdcb39b2d
yusuf-delvi <yusufmddelvi8@gmail.com>
2024-10-19 19:14:18 +0530

    add header with logout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
+import { useAuth } from '@/context/AuthContext';
 import withAuth from '@/hoc/withAuth';
 
 function Home() {
-	return <div>Home page</div>;
+	const { removeJWTToken } = useAuth();
+
+	return (
+		<>
+			<header className='shadow flex items-center justify-between px-9 py-2'>
+				<h1 className='text-xl font-semibold'>Tasks</h1>
+				<button
+					onClick={() => removeJWTToken()}
+					className='text-white bg-violet-900 hover:bg-violet-950 px-3 py-1 rounded'
+				>
+					Logout
+				</button>
+			</header>
+			<div>Home page</div>
+		</>
+	);
 }
 
 export default withAuth(Home);
